8c3d90461697b11f5837257e967dd1021f3fa551
Ali <alialhamadani72@gmail.com>
2023-11-19 01:13:14 -0500

    Finished data processing, switched to minmax normalization.
    To use the class, simply create an ECGRecord object and pass in the record number. Then, convert it to an ECGData object. Now, there are two batches generated, one for individual beats and one for joined beats. Call the corresponding 'k-fold validation' function, and it will generate an xtrain, xtest, ytrain and ytest for you.

--- ml-model/ecgdata.py
+++ ml-model/ecgdata.py
@@ -5,6 +5,8 @@ import math
 import numpy as np
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+import random
+
 
 """
 Object of Neural Network Input Data
@@ -55,9 +57,17 @@ class ECGData:
     self.j_x_batch = []
     self.j_y_batch = []
 
+    self.prepare_batch(0.4)
 
-  def prepare_batch(self):
+
+  def prepare_batch(self, irregularity_split=0.5):
     print("Preparing batch...")
+
+    full_i_x_batch = []
+    full_i_y_batch = []
+    full_j_x_batch = []
+    full_j_y_batch = []
+
 
     # Individual beats batch
     assert len(self.i_rhythm_batch_values) == len(self.i_rhythm_batch_labels)
@@ -65,20 +75,66 @@ class ECGData:
       b_list = list(b_rhyth[:,:,1])
       l_list = list(l_rhyth)
 
-      for i, l in enumerate(l_list): l_list[i] = 0 if l == 'N' else 1
+      for i, l in enumerate(l_list): l_list[i] = (0 if l == 'N' else 1)
+      for i, b in enumerate(b_list): b_list[i] = b.reshape((b.shape[0], 1))
 
-      self.i_x_batch.extend(b_list)
-      self.i_y_batch.extend(l_list)
+      full_i_x_batch.extend(b_list)
+      full_i_y_batch.extend(l_list)
 
     assert len(self.j_rhythm_batch_values) == len(self.j_rhythm_batch_labels)
     for b_rhyth, l_rhyth in zip(self.j_rhythm_batch_values, self.j_rhythm_batch_labels):
       b_list = list(b_rhyth[:,:,1])
       l_list = list(l_rhyth)
       
-      for i, l in enumerate(l_list): l_list[i] = 0 if l == 'N' else 1
+      for i, l in enumerate(l_list): l_list[i] = (0 if l == 'N' else 1)
+      for i, b in enumerate(b_list): b_list[i] = b.reshape((b.shape[0], 1))
+
         
-      self.j_x_batch.extend(b_list)
-      self.j_y_batch.extend(l_list)
+      full_j_x_batch.extend(b_list)
+      full_j_y_batch.extend(l_list)
+
+    # irregularity split goes here
+
+
+    i_only_irreg = [(elem,1) for i, elem in enumerate(full_i_x_batch) if (full_i_y_batch[i])]
+    i_only_normal = [(elem,0) for i, elem in enumerate(full_i_x_batch) if (not full_i_y_batch[i])]
+    
+    j_only_irreg = [(elem,1) for i, elem in enumerate(full_j_x_batch) if (full_j_y_batch[i])]
+    j_only_normal = [(elem,0) for i, elem in enumerate(full_j_x_batch) if (not full_j_y_batch[i])]
+
+    i_k_normals = int(len(i_only_irreg)/irregularity_split) - len(i_only_irreg)
+    i_normals = random.choices(i_only_normal, k=i_k_normals)
+
+    j_k_normals = int(len(j_only_irreg)/irregularity_split) - len(j_only_irreg)
+    j_normals = random.choices(j_only_normal, k=j_k_normals)
+
+    # join normals and irregulars
+    i_combined = []
+    i_combined.extend(i_normals)
+    i_combined.extend(i_only_irreg)
+
+    j_combined = []
+    j_combined.extend(j_normals)
+    j_combined.extend(j_only_irreg)
+
+    random.shuffle(i_combined)
+    random.shuffle(j_combined)
+
+
+    for i in range(0, len(i_combined)):
+      self.i_x_batch.append(i_combined[i][0])
+      self.i_y_batch.append(i_combined[i][1])
+
+    for i in range(0, len(j_combined)):
+      self.j_x_batch.append(j_combined[i][0])
+      self.j_y_batch.append(j_combined[i][1])
+
+    self.i_x_batch = np.array(self.i_x_batch).astype('f')
+    self.i_y_batch = np.array(self.i_y_batch).astype('i')
+    
+    self.j_x_batch = np.array(self.j_x_batch).astype('f')
+    self.j_y_batch = np.array(self.j_y_batch).astype('i')
+
 
   def show_individual_beat(self, only_irregularities=False):
     print("\n\n\nTEST")
@@ -101,12 +157,94 @@ class ECGData:
       y = self.j_x_batch[i]
 
       if only_irregularities and self.j_y_batch[i] == "N": continue
-      print("\n\nLABEL:")
-      print(self.j_y_batch[i])
       stop = len(y)/self.sample_frequency
       x = np.linspace(0, stop, self.joined_num_samples)
 
       plt.plot(x, y)
       plt.title(f"Label: {self.j_y_batch[i]}")
       plt.show()
+
+
+
+  def j_fold_validation_split(self, test_i:int, k=3):
+    """
+    Returns x_train, y_train, x_test, y_test
+    """
+    extra = len(self.j_x_batch) % k
+    x_batch_trimmed = self.j_x_batch if extra == 0 else self.j_x_batch[:-extra]
+    y_batch_trimmed = self.j_y_batch if extra == 0 else self.j_y_batch[:-extra]
+
+    x_batch_trimmed.astype('f')
+    y_batch_trimmed.astype('i')
+
+    print(x_batch_trimmed)
+    print(len(y_batch_trimmed))
+
+    size_fold = math.floor(len(self.j_x_batch)/k)
+    print(f"Fold Size: {size_fold}")
+
+    #x_train, x_test, y_train, y_test = None
+    train_assigned = False
+    for i in range(0, len(x_batch_trimmed), size_fold):
+      index = int(i/size_fold)
+      print(i, "  ", index)
+
+      if index == test_i:
+        print("Test fold")
+        x_test = x_batch_trimmed[i:i+size_fold]
+        y_test = y_batch_trimmed[i:i+size_fold]
+
+      else:
+        print("Train fold")
+        if not train_assigned:
+          train_assigned = True
+          x_train = x_batch_trimmed[i:i+size_fold]
+          y_train = y_batch_trimmed[i:i+size_fold]
+        else:
+          np.concatenate((x_train, x_batch_trimmed[i:i+size_fold]), axis=0, dtype='f')
+          np.concatenate((y_train, y_batch_trimmed[i:i+size_fold]), axis=0, dtype='i')
+
+    return x_train, x_test, y_train, y_test
+
+  def i_fold_validation_split(self, test_i:int, k=3):
+      """
+      Returns x_train, y_train, x_test, y_test
+      """
+      extra = len(self.i_x_batch) % k
+      x_batch_trimmed = self.i_x_batch if extra == 0 else self.i_x_batch[:-extra]
+      y_batch_trimmed = self.i_y_batch if extra == 0 else self.i_y_batch[:-extra]
+
+      x_batch_trimmed.astype('f')
+      y_batch_trimmed.astype('i')
+
+      print(x_batch_trimmed)
+      print(len(y_batch_trimmed))
+
+      size_fold = math.floor(len(self.j_x_batch)/k)
+      print(f"Fold Size: {size_fold}")
+
+      #x_train, x_test, y_train, y_test = None
+      train_assigned = False
+      for i in range(0, len(x_batch_trimmed), size_fold):
+        index = int(i/size_fold)
+        print(i, "  ", index)
+
+        if index == test_i:
+          print("Test fold")
+          x_test = x_batch_trimmed[i:i+size_fold]
+          y_test = y_batch_trimmed[i:i+size_fold]
+
+        else:
+          print("Train fold")
+          if not train_assigned:
+            train_assigned = True
+            x_train = x_batch_trimmed[i:i+size_fold]
+            y_train = y_batch_trimmed[i:i+size_fold]
+          else:
+            np.concatenate((x_train, x_batch_trimmed[i:i+size_fold]), axis=0, dtype='f')
+            np.concatenate((y_train, y_batch_trimmed[i:i+size_fold]), axis=0, dtype='i')
+
+      return x_train, x_test, y_train, y_test
+
+
 
--- ml-model/record.py
+++ ml-model/record.py
@@ -107,7 +107,7 @@ class ECGRecord:
     self.__rhythm_batch: list[pd.DataFrame] = batch
   
 
-  def __split_rhythm(self, rhythm: pd.DataFrame, normalize, interval_length:float = 1, num_beats_joined = 5, num_samples_joined=1360) -> tuple[np.ndarray, np.ndarray]:
+  def __split_rhythm(self, rhythm: pd.DataFrame, normalize, interval_length:float = 1, num_beats_joined = 3, num_samples_joined=815) -> tuple[np.ndarray, np.ndarray]:
     """
     Split a rhythm into nested lists, each representing a beat centered around the annotation sample
 
@@ -160,12 +160,13 @@ class ECGRecord:
     # convert sample index to int
     indiv_beats_values[:,:,0].astype('int')
 
-    for i in range(0, len(indiv_beats_values)-4, 5):
+    for i in range(0, len(indiv_beats_values)-(num_beats_joined-1), num_beats_joined):
       start_sample = indiv_beats_values[i][0][0]
-      end_sample = indiv_beats_values[i+4][len(indiv_beats_values[i+4])-1][0]
+      end_sample = indiv_beats_values[i+(num_beats_joined-1)][len(indiv_beats_values[i+(num_beats_joined-1)])-1][0]
 
       diff_samples = end_sample-start_sample
       error = diff_samples - num_samples_joined + 1
+      #assert error > 0, f"Diff samples: {diff_samples}, Num samples: {num_samples_joined}"
 
       joined_df: pd.DataFrame = rhythm.loc[(rhythm["sample"] >= (start_sample + math.floor(error/2))) & (rhythm["sample"] <= (end_sample - math.ceil(error/2)))]
       
@@ -190,24 +191,25 @@ class ECGRecord:
     # Normalize Data
     # Add the abs of the lowest negative value to each entry such that all values are now positive
     if normalize:
+      scaler = preprocessing.MinMaxScaler()
       indiv_beats_values[:,:,1] += abs(np.min(indiv_beats_values[:,:,1]))
-      indiv_beats_values[:,:,1] = preprocessing.normalize(indiv_beats_values[:,:,1])
+      indiv_beats_values[:,:,1] = scaler.fit_transform(indiv_beats_values[:,:,1])
 
       indiv_beats_values[:,:,2] += abs(np.min(indiv_beats_values[:,:,2]))
-      indiv_beats_values[:,:,2] = preprocessing.normalize(indiv_beats_values[:,:,2])
+      indiv_beats_values[:,:,2] = scaler.fit_transform(indiv_beats_values[:,:,2])
 
       joined_beats_values[:,:,1] += abs(np.min(joined_beats_values[:,:,1]))
-      joined_beats_values[:,:,1] = preprocessing.normalize(joined_beats_values[:,:,1])
+      joined_beats_values[:,:,1] = scaler.fit_transform(joined_beats_values[:,:,1])
 
       joined_beats_values[:,:,2] += abs(np.min(joined_beats_values[:,:,2]))
-      joined_beats_values[:,:,2] = preprocessing.normalize(joined_beats_values[:,:,2])
+      joined_beats_values[:,:,2] = scaler.fit_transform(joined_beats_values[:,:,2])
 
     indiv_beats_labels = np.array(indiv_beats_labels)
     joined_beats_labels = np.array(joined_beats_labels)
     return indiv_beats_values, indiv_beats_labels, joined_beats_values, joined_beats_labels
 
 
-  def __split_rhythm_batch(self, normalize=False, join=5, joined_samples = 1360) -> tuple[list[np.ndarray], list[np.ndarray]]:
+  def __split_rhythm_batch(self, join, joined_samples, normalize=False) -> tuple[list[np.ndarray], list[np.ndarray]]:
     """
     Split a rhythm batch into a list of beats
 
@@ -225,7 +227,7 @@ class ECGRecord:
 
     for rhythm in rhythm_batch:
       if len(rhythm) < (360*join): continue
-      indiv_beats_values, indiv_beats_labels, joined_beats_values, joined_beats_labels = self.__split_rhythm(rhythm, normalize, interval_length=1, num_beats_joined=join, num_samples_joined=joined_samples)
+      indiv_beats_values, indiv_beats_labels, joined_beats_values, joined_beats_labels = self.__split_rhythm(rhythm, normalize=normalize, interval_length=1, num_beats_joined=join, num_samples_joined=joined_samples)
       individual_batch_values.append(indiv_beats_values)
       individual_batch_labels.append(indiv_beats_labels)
       joined_batch_values.append(joined_beats_values)
@@ -244,9 +246,12 @@ class ECGRecord:
   (sample_index, upper_lead, lower_lead)
   """
   
+  # 3 beats -> 720
+  # 5 beats -> 1350
+
   def to_ecg_data(self, normalize=True) -> ECGData:
-    rhythm_indiv_batch_values, rhythm_indiv_batch_labels, rhythm_joined_batch_values, rhythm_joined_batch_labels = self.__split_rhythm_batch(normalize, join=5, joined_samples=1360)
+    rhythm_indiv_batch_values, rhythm_indiv_batch_labels, rhythm_joined_batch_values, rhythm_joined_batch_labels = self.__split_rhythm_batch(normalize=normalize, join=5, joined_samples=1350)
     return ECGData(rhythm_indiv_batch_values, rhythm_indiv_batch_labels,
                    rhythm_joined_batch_values, rhythm_joined_batch_labels, 
-                   self.beat_num_samples, 1360, 5 ,self.sample_frequency)
+                   self.beat_num_samples, 1350, 5 ,self.sample_frequency)
 
